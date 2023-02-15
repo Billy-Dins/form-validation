2ac5989e8485211c1bd53e892ba58a6c12241aa2
Billy <Lrdweaver@gmail.com>
2023-02-15 06:31:34 -0500

    added validation for postal codes

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ const passwordError = document.querySelector('#passwordError')
 
 function showError(input, error) {
     if (input.validity.valueMissing){
-        error.textContent = 'You need to enter an email address'
+        error.textContent = 'You need to enter a value'
     } else if (input.validity.typeMismatch) {
-        error.textContent = 'entered value needs to be an email address'
+        error.textContent = 'input needs to be requested type'
     } else if (input.validity.tooShort) {
-        error.textContent = `Email should be atleast ${emailInput.minLength} characters; you entered ${emailInput.value.length}.`
+        error.textContent = `input should be atleast ${input.minLength} characters; you entered ${input.value.length}.`
+    } else if (input.validity.patternMismatch) {
+        error.textContent = 'please entern a valid postal code in the format "A0A 0A0"'
     }
     error.className = 'error active'
 }
@@ -30,3 +32,29 @@ emailInput.addEventListener('input', (event) => {
     }
 })
 
+countryInput.addEventListener('input', (event) => {
+    if (countryInput.validity.valid) {
+        countryError.textContent = '';
+        countryError.className = 'error';
+    } else {
+        showError(countryInput, countryError)
+    }
+})
+
+zipInput.addEventListener('input', (event) => {
+    if (zipInput.validity.valid) {
+        zipError.textContent = '';
+        zipError.className = 'error'
+    }  else {
+        showError(zipInput, zipError)
+    }
+})
+
+passwordInput.addEventListener('input', (event) => {
+    if (passwordInput.validity.valid) {
+        passwordError.textContent = '';
+        passwordError.className = 'error'
+    } else {
+        showError(passwordInput, passwordError)
+    }
+})
