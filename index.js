a32c4be2431e02eb6d9e30bb4bed3954aa792893
Billy <Lrdweaver@gmail.com>
2023-02-15 06:17:19 -0500

    form with js validity for email

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-import hello from './src/form.js'
-
 import style from './src/style.css'
 
-hello();
+const form = document.querySelector('#browser-form')
+const emailInput = document.querySelector('#email')
+const emailError = document.querySelector('#emailError')
+const countryInput = document.querySelector('#country')
+const countryError = document.querySelector('#countryError')
+const zipInput = document.querySelector('#zip')
+const zipError = document.querySelector('#zipError')
+const passwordInput = document.querySelector('#password')
+const passwordError = document.querySelector('#passwordError')
+
+function showError(input, error) {
+    if (input.validity.valueMissing){
+        error.textContent = 'You need to enter an email address'
+    } else if (input.validity.typeMismatch) {
+        error.textContent = 'entered value needs to be an email address'
+    } else if (input.validity.tooShort) {
+        error.textContent = `Email should be atleast ${emailInput.minLength} characters; you entered ${emailInput.value.length}.`
+    }
+    error.className = 'error active'
+}
+
+emailInput.addEventListener('input', (event) => {
+    if (emailInput.validity.valid) {
+        emailError.textContent = '';
+        emailError.className = 'error'
+    } else {
+        showError(emailInput, emailError);
+    }
+})
+
--- src/form.js
+++ src/form.js
@@ -1,5 +1,12 @@
-function hello() {
-    console.log('hello')
-}
+const form = document.querySelector('#browser-form')
+const emailInput = document.querySelector('#email')
+const countryInput = document.querySelector('#country')
+const zipInput = document.querySelector('#zip')
+const passwordInput = document.querySelector('#password')
 
-export default hello
+emailInput.addEventListener('input', (event) => {
+    console.log('hello')
+    if (emailInput.validity.typeMismatch) {
+        console.log('hello')
+    }
+})
