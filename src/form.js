99cd3937f4c5915e0b4762bcff36f91612d7f5c0
Billy <Lrdweaver@gmail.com>
2023-02-16 05:56:51 -0500

    styling CSS

--- src/form.js
+++ src/form.js
@@ -4,9 +4,4 @@ const countryInput = document.querySelector('#country')
 const zipInput = document.querySelector('#zip')
 const passwordInput = document.querySelector('#password')
 
-emailInput.addEventListener('input', (event) => {
-    console.log('hello')
-    if (emailInput.validity.typeMismatch) {
-        console.log('hello')
-    }
-})
+
